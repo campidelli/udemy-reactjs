832bf20664bd93431a59b0115993786ccf7136ad
Bruno Campidelli <campidelli@gmail.com>
2020-03-09 16:07:53 +1300

    Class 78 done.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ShortHaulExportFilter from './components/ShortHaulExportFilter';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<ShortHaulExportFilter />, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is my ADD component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is my EDIT component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Heeeeeelp \o/
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404 =/
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
