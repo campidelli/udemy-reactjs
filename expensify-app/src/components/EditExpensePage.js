e125e7a8a814784a68bc8957067835a5af8739cb
Bruno Campidelli <campidelli@gmail.com>
2020-03-09 16:47:39 +1300

    Class 80 done.

---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>
+    This is my EDIT component
+  </div>
+);
+
+export default EditExpensePage;
---
+++ expensify-app/src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+  <div>
+    Heeeeeelp \o/
+  </div>
+);
+
+export default HelpPage;
